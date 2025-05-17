1bfdf038b91b03160d845b4204e76f180bc0195c
Xime <xebertz@campus.ungs.edu.ar>
2025-05-17 17:57:37 -0300

    Fix setup

--- setup.py
+++ setup.py
@@ -20,30 +20,30 @@ create_tables(conn)
 
 ####################################################################
 # Centros de fabricación
-centro_de_fabricacion_insert_statement = "insert into centro_de_fabricacion (nombre, data) values (%s, %s)"
-centro_de_fabricacion_csv_file = "./db/data/conjuntos/centros_de_fabricacion.csv"
-insert_data_from_csv(conn, centro_de_fabricacion_insert_statement, centro_de_fabricacion_csv_file)
+fabrication_center_insert_statement = "insert into centro_de_fabricacion (nombre, data) values (%s, %s)"
+fabrication_center_csv_file = "./db/data/conjuntos/fabrication_centers.csv"
+insert_data_from_csv(conn, fabrication_center_insert_statement, fabrication_center_csv_file)
 ####################################################################
 
 ####################################################################
 # Centros de distribución
-centro_de_distribucion_insert_statement = "insert into centro_de_distribucion (nombre, data) values (%s, %s)"
-centro_de_distribucion_csv_file = "./db/data/conjuntos/centros_de_distribucion.csv"
-insert_data_from_csv(conn, centro_de_distribucion_insert_statement, centro_de_distribucion_csv_file)
+distribution_center_insert_statement = "insert into centro_de_distribucion (nombre, data) values (%s, %s)"
+distribution_center_csv_file = "./db/data/conjuntos/distribution_centers.csv"
+insert_data_from_csv(conn, distribution_center_insert_statement, distribution_center_csv_file)
 ####################################################################
 
 ####################################################################
 # Puntos de venta
-punto_de_venta_insert_statement = "insert into punto_de_venta(nombre, data) values (%s, %s)"
-punto_de_venta_csv_file = "./db/data/conjuntos/puntos_de_venta.csv"
-insert_data_from_csv(conn, punto_de_venta_insert_statement, punto_de_venta_csv_file)
+point_of_sale_insert_statement = "insert into punto_de_venta(nombre, data) values (%s, %s)"
+point_of_sale_csv_file = "./db/data/conjuntos/points_of_sale.csv"
+insert_data_from_csv(conn, point_of_sale_insert_statement, point_of_sale_csv_file)
 ####################################################################
 
 ####################################################################
 # Escenarios
-escenario_insert_statement = "insert into escenario (nombre, data) values (%s, %s)"
-escenario_csv_file = "./db/data/conjuntos/escenarios.csv"
-insert_data_from_csv_json(conn, escenario_insert_statement, escenario_csv_file)
+scenario_insert_statement = "insert into escenario (nombre, data) values (%s, %s)"
+scenario_csv_file = "./db/data/conjuntos/scenarios.csv"
+insert_data_from_csv_json(conn, scenario_insert_statement, scenario_csv_file)
 ####################################################################
 
 conn.close()
@@ -52,8 +52,8 @@ print("[okay] Connection closed")
 ####################################################################
 
 # Mock
-a = input("Do you want to dump (backup to a file) the database? (y/n): ")
-dump("db/data/supply_chain_dump.sql")
+# a = input("Do you want to dump (backup to a file) the database? (y/n): ")
+# dump("db/data/supply_chain_dump.sql")
 
-a = input("Do you want to restore the database from the dump file? (y/n): ")
-restore("db/data/supply_chain_dump.sql")
+# a = input("Do you want to restore the database from the dump file? (y/n): ")
+# restore("db/data/supply_chain_dump.sql")
