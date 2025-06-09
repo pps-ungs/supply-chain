df613fb1adc6c842530413cbff9d5ce1bd133fcd
Xime <xebertz@campus.ungs.edu.ar>
2025-06-09 14:04:41 -0300

    Refactor > call to model

--- experiments/x_initial_main.py
+++ experiments/x_initial_main.py
@@ -5,13 +5,17 @@ import sys
 import time
 import inspect
 
+from models.hill_climbing import HillClimbing
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../db/')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../models/')))
 
 import db.config as dbconfig
 import db.database as db
 import models.model as model
 import x_initial
+import hill_climbing
 import neighborhood
 import json
 
@@ -139,7 +143,6 @@ def optimization_heuristic_initial_x(F: list, S: list, P: list, E: list, step: f
         "time": total_time
     }
 
-# ESTA HARDCODEADA LA DISTRIBUCION
 def log_optimization_heuristic(experiment, X_initial, Z_initial, X, Z, step, it, actual_time, halting_condition, strategy):
     conn = db.get_connection({
         "user": "postgres",
@@ -190,87 +193,40 @@ def optimization_heuristic(
         epsilon: float = 1e-12,
         max_iterations_allowed: int = 1e12,
         max_stuck_allowed: int = 1e3) -> dict:
-
-    X_initial, Z_initial = initial_obj
-
-    X_current = X_initial
-    Z_current = Z_initial
-
-    it = 0
-    stuck = 0
-
-    ####################################################################
-    # Criterios de parada
-    Z_current_is_better = True
-    limit_is_not_reached = True
-    is_not_stuck = True
-    ####################################################################
     
-    initial_time = time.time()
+    intial_time = time.time()
+    
+    model = HillClimbing(F, S, P, E)
+    result = model.solve(
+        step=step,
+        epsilon=epsilon,
+        max_iterations_allowed=max_iterations_allowed,
+        max_stuck_allowed=max_stuck_allowed,
+        initial_X = initial_obj[0]
+    )
 
-    while Z_current_is_better and limit_is_not_reached and is_not_stuck:
-        # first improvement - multi change, con un step 20, 100.000 iteraciones, y 32 vecinos. 
-        # neighbors = neighborhood.create_exhaustive_neighbors(X_current, step, 32)
-        neighbors = neighborhood.create_multi_change_neighbors(X_current, step, 64)
-
-        evaluated_neighbors = [(n, model.get_objective_value(F, S, P, E, n)) for n in neighbors]
-
-        # Evaluation of the neighbourhood
-        best_n, best_z = neighborhood.greedy_selection_eval(evaluated_neighbors, Z_current)
-
-        # Comparing the best solution with the current one
-        if best_n is not None:
-            X_current = best_n
-            Z_previous = Z_current
-            Z_current = best_z
-
-        ################################################################
-        # Criterios de parada
-        #
-        # 1. Número máximo de iteraciones:
-        it += 1
-        limit_is_not_reached = it < max_iterations_allowed
-        #
-        # 2. Estancamiento:
-        if Z_current == Z_previous:
-            stuck += 1
-            print(f"[warning] stuck in local optimum {Z_current} for {stuck} iterations")
-        elif Z_current > Z_previous:
-            stuck = 0
-            # 3. Mejora entre las iteraciones
-            #
-            # Si la mejora es mayor a epsilon, se considera que la
-            # solución actual es mejor que la anterior, y se sigue
-            # buscando.
-            Z_current_is_better = abs(Z_current - Z_previous) > epsilon
-        else: # Z_current < Z_previous
-            # Este es el caso en el que la solución anterior es mejor.
-            # Nosotros usamos greedy local search, por ende, no se
-            # debería dar este caso. Pero si se da, es porque la
-            # solución anterior es mejor que la actual.
-            stuck = 0
-            raise Exception(f"?previous objective value {Z_previous} is better than the current one {Z_current}")
-
-        if Z_current < 0:
-            print(f"[warning] current objective value is negative: {Z_current}")
-
-        is_not_stuck = stuck < max_stuck_allowed
-        ################################################################
-
-    halting_condition = None
-    if not Z_current_is_better:
-        halting_condition = "Satisfactory solution found"
-    elif not limit_is_not_reached:
-        halting_condition = "Maximum number of iterations"
-    elif not is_not_stuck:
-        halting_condition = "Stuck in local optimum"
-
-    actual_time = time.time() - initial_time
-    log_f(experiment, X_initial, Z_initial, X_current, Z_current, step, it, actual_time, halting_condition, strategy)
+    actual_time = time.time() - intial_time
+    
+    X = result["X"]
+    Z = result["Z"]
+    halting_condition = result.get("halting_condition", "unknown")
+    
+    log_f(
+        experiment=experiment,
+        X_initial=initial_obj[0],
+        Z_initial=initial_obj[1],
+        X=X,
+        Z=Z,
+        step=step,
+        it=result.get("iterations", 0),
+        actual_time=actual_time,
+        halting_condition=halting_condition,
+        strategy=strategy
+    )
 
     return {
-        "X": X_current,
-        "Z": Z_current,
+        "X": X,
+        "Z": Z,
         "time": actual_time,
         "halting_condition": halting_condition
     }
