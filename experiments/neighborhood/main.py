6880aa7a86791334fc93203f53ee8e681bff2b37
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-26 09:17:20 -0300

    Fix neighborhood imports

--- experiments/neighborhood/main.py
+++ experiments/neighborhood/main.py
@@ -3,13 +3,11 @@
 import os
 import sys
 
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../db")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../models")))
+sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../models/')))
 
 import db.config as dbconfig
-import experiments.neighborhood.run_experiments as nh
+from experiments.neighborhood import run_experiments as nh
 import setup
 import models.hill_climbing as hill_climbing
 
@@ -23,8 +21,6 @@ def main():
     model = hill_climbing.HillClimbing(F, S, P, E)
 
     dir = "results_new_db/creation_evaluation_neighbors"
-    # nh.run_creation_neighbors_experiment(dir=dir, model=model)
-    # nh.run_eval_neighbors_experiment(dir=dir, model=model)
     nh.run_creation_eval_neighbors_experiment(dir=dir, model=model)
 
 
