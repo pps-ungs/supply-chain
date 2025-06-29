e230343de80ca5092e30b1539940b09f83656d3e
Lucia <127611277+LuciaSoria5@users.noreply.github.com>
2025-06-29 20:44:09 -0300

    Add function to run aco

--- gui/SUPPAI_support.py
+++ gui/SUPPAI_support.py
@@ -1,19 +1,42 @@
 #! /usr/bin/env python3
+import os
 import sys
+
+sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../models/')))
+
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter.constants import *
+import db.config as dbconfig
+import setup
+from models.ant_colony import AntColony
 
 import SUPPAI
 
 _debug = True
 
-# TODO: Add a function to connect to the database
 def connect_to_database():
-    print("Viva Perón!")
+    config = dbconfig.load_config('db/database.ini', 'supply_chain')
+    data = setup.read_database(config)
+    return data["F"], data["S"], data["P"], data["E"]
+ 
 
 def get_active_heuristic():
     return "Aguante Cristina!"
+
+def run_aco(alpha, beta, rho, Q, tau_max, tau_min, num_prod_levels, num_ants, num_iterations):
+    F, S, P, E = connect_to_database()
+    model_aco = AntColony(F, S, P, E, 
+                    alpha=alpha, 
+                        beta=beta, 
+                        rho=rho,
+                        Q= Q if Q else None,
+                        tau_max= tau_max if tau_max else None,
+                        tau_min= tau_min if tau_min else None,
+                        num_prod_levels=num_prod_levels)
+
+    return model_aco.solve(num_ants=num_ants, max_iterations=num_iterations)
 
 def main(*args):
     global root
